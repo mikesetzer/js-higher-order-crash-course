66228bc035f237cacaa8933fe03a79de416ab658
Mike Setzer <michaelsetzerucf@gmail.com>
2024-03-14 18:19:57 -0400

    Done

--- main.js
+++ main.js
@@ -43,8 +43,52 @@ const retailCompanies = companies.filter(company => company.category === 'Retail
 // Get 80s Companies
 const eightiesCompanies = companies.filter(company => company.start >= 1980 && company.start < 1990)
 
-console.log(eightiesCompanies);
+// Get companies that lasted 10 years or more
+
+const lastedTenYears = companies.filter(company => (company.end - company.start ) >= 10 ); 
 
 // map
+// Create array of company names
+const companyNames = companies.map(company => `${company.name} [${company.start} - ${company.end}]`);
+
+// Create array of square root of all ages
+const agesSquare = ages.map(age => Math.sqrt(age));
+
 // sort
+// Sort companies by start year
+// const sortedCompanies = companies.sort(function(c1, c2) {
+//     if(c1.start > c2.start){
+//         return 1;
+//     } else {
+//         return -1;
+//     }
+// });
+
+// const sortedCompanies = companies.sort((a, b) => (a.start > b.start) ? 1 : -1);
+
+//const sortAges = ages.sort((a, b) => (a > b ) ? 1 : -1);
+
 // reduce
+// Add all ages together
+// let ageSum = 0;
+// for(let i = 0; i < ages.length; i++){
+//     ageSum += ages[i];
+// }
+
+// const ageSum = ages.reduce(function(total, age) {
+//     return total + age;
+// }, 0);
+
+//const ageSum = ages.reduce((total, age) => total + age, 0);
+
+// Get total years for all companies
+const totalYears = companies.reduce((total, company) => total + (company.end - company.start), 0);
+
+//Combine Methods
+const combined = ages
+    .map(age => age * 2)
+    .filter(age => age >= 40)
+    .sort((a, b) => (a - b))
+    .reduce((a, b) => a + b, 0);
+
+console.log(combined);
